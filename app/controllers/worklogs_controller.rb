c5daa495a23cb07296037980417468cb319159a1
Evgeny Esaulkov <evgeniy.esaulkov@flatstack.com>
2023-09-03 12:40:10 +0200

    display tracked time for the current week

--- app/controllers/worklogs_controller.rb
+++ app/controllers/worklogs_controller.rb
@@ -2,5 +2,19 @@
 
 class WorklogsController < ApplicationController
   def index
+    @start_date = Date.current.beginning_of_week
+    @end_date = Date.current.end_of_week
+
+    @grouped_worklogs = worklogs.group_by { |w| [w.project, w.activity] }
+  end
+
+  private
+
+  def worklogs
+    return [] if current_user.employees.none?
+
+    current_user.employees.first.worklogs
+      .includes(:project, :activity)
+      .where(work_date: @start_date..@end_date)
   end
 end
